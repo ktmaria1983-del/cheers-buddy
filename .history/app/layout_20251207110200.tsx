e843aaa2173ae21acfa0b4764375f7844e41bdb2
Tunde <ktmaria1983@gmail.com>
2025-12-07 11:15:47 +0000

    update

--- .history/app/layout_20251207110200.tsx
+++ .history/app/layout_20251207110200.tsx
@@ -34,8 +34,6 @@ export default function RootLayout({
       >
         {/* Navbar stays at the top */}
         <NavBar />
-        <CommunityPopup />
-
 
         {/* Community popup appears globally */}
         {/* <CommunityPopup /> */}
